af324fe157cb1d5afde34b8c40ac860e09e553e3
Imane <149276102+ImaneElla@users.noreply.github.com>
2026-02-05 00:32:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import ProjectDetails from "./pages/ProjectDetails";
 import CreateProject from "./pages/CreateProject";
 import "./App.css";
 
@@ -9,7 +8,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/projects/:id" element={<ProjectDetails />} />
         <Route path="/create" element={<CreateProject />} />
       </Routes>
     </BrowserRouter>
